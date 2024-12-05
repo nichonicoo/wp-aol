a474470ce08037c40cf5e62c30f321d5592e68a1
Samuel Theophylus Wieguna <wiegunasamuel@gmail.com>
2024-12-05 16:02:28 +0700

    GambarUdah Bisa

--- aol/resources/views/AllReports.blade.php
+++ aol/resources/views/AllReports.blade.php
@@ -121,7 +121,7 @@
         <ul class="grid gap-x-8 gap-y-10 mt-16 sm:grid-cols-2 lg:grid-cols-3 justify-center mx-auto">
             @foreach ($datas2 as $data)
                 <li class="w-full mx-auto group sm:max-w-sm">
-                    {{-- <img src="{{ asset('images/' . $data->photo_url) }}" loading="lazy" alt="{{ $data->Title }}" class="w-full rounded-lg" /> --}}
+                    <img src="{{ asset('storage/' . $data->photo_url) }}" loading="lazy" alt="{{ $data->Title }}" class="w-full rounded-lg" />
                     <div class="mt-3 space-y-2">
                         <span class="block text-indigo-600 text-sm">{{ \Carbon\Carbon::parse($data->created_at)->format('M d, Y') }}</span>
                         <h3 class="text-lg text-gray-800 duration-150 group-hover:text-indigo-600 font-semibold">{{ $data->Title }}</h3>
