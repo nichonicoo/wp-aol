0e74ab95afb19703429b5484ec11b5fcca8360e5
Nicholas <128897824+nichonicoo@users.noreply.github.com>
2024-12-08 20:13:16 +0700

    test1

--- aol/tailwind.config.js
+++ aol/tailwind.config.js
@@ -4,9 +4,14 @@ import forms from '@tailwindcss/forms';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
+        //test
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        // './storage/framework/views/*.php',
+        // './resources/views/**/*.blade.php',
+
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
 
     theme: {
